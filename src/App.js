7113510834822af7cf97d7d20fe06a2513d8daee
jaspreet kaur <jaspreetkaur16june@gmail.com>
2021-11-06 15:50:16 +0530

    Practice of Material UI Concepts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import {Typography, AppBar, Toolbar, Card, CardContent, CardActions, CardMedia, CssBaseline, Grid, Container} from "@material-ui/core"
+import { PhotoCamera } from "@material-ui/icons";
 
 let App=()=> {
   return (
-    <div className="App">
-     
-    </div>
+    <>
+     <CssBaseline />
+     <AppBar>
+       <Toolbar position = "relative">
+       <PhotoCamera />
+       <Typography variant = "h6">Photo Album</Typography>
+       </Toolbar>
+     </AppBar>
+    </>
   );
 }
 
